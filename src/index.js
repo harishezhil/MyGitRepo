48e4851376b858843ad5bd3fcf8b6902c4877638
harishezhil <146364514+harishezhil@users.noreply.github.com>
2025-01-20 00:02:03 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // You can style your app here, or leave it empty if you prefer.
+
+import App from './App'; // Assuming you have an App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
